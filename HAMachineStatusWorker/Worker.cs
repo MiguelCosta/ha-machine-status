0298b49622c65764eaf05a51f1b3ee503721ea95
Pedro Miguel Fernandesq <pedro@pmfernandes.com>
2022-11-30 23:22:40 +0000

    feat: add sensor creation

--- HAMachineStatusWorker/Worker.cs
+++ HAMachineStatusWorker/Worker.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using MQTTnet;
 using MQTTnet.Client;
 
@@ -23,6 +24,8 @@ public class Worker : BackgroundService
     {
         await ConnectClientAsync();
 
+        await this.CreateSensors();
+
         while (!stoppingToken.IsCancellationRequested)
         {
             var status = new
@@ -34,6 +37,21 @@ public class Worker : BackgroundService
             _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
             _logger.LogInformation("OS Version: {time}", status.OsVersion);
             _logger.LogInformation("Uptime: {time}", status.Uptime);
+
+            var applicationMessage = new MqttApplicationMessageBuilder()
+                .WithTopic("machinestatus/os_version")
+                .WithPayload(status.OsVersion)
+                .Build();
+
+            await this._mqttClient.PublishAsync(applicationMessage, CancellationToken.None);
+
+            applicationMessage = new MqttApplicationMessageBuilder()
+                .WithTopic("machinestatus/uptime")
+                .WithPayload(status.Uptime.ToString())
+                .Build();
+
+            await this._mqttClient.PublishAsync(applicationMessage, CancellationToken.None);
+
             await Task.Delay(5000, stoppingToken);
         }
 
@@ -69,5 +87,66 @@ public class Worker : BackgroundService
         {
             _logger.LogError(ex, "MQTT ERROR");
         }
+    }
+
+    private async Task CreateSensors()
+    {
+        var applicationMessage = new MqttApplicationMessageBuilder()
+                .WithTopic("homeassistant/sensor/MachineStatus/OSVersion/config")
+                .WithPayload(GetVersionSensor())
+                .Build();
+
+        await this._mqttClient.PublishAsync(applicationMessage, CancellationToken.None);
+
+        applicationMessage = new MqttApplicationMessageBuilder()
+                .WithTopic("homeassistant/sensor/MachineStatus/Uptime/config")
+                .WithPayload(GetUptimeSensor())
+                .Build();
+
+        await this._mqttClient.PublishAsync(applicationMessage, CancellationToken.None);
+    }
+
+    private static string GetVersionSensor()
+    {
+        var sensor = new
+        {
+            name = "OS Version",
+            state_topic = "machinestatus/os_version",
+            icon = "mdi:desktop-classic",
+            retain = true,
+            unique_id = "66de0f5a-f0f3-4f39-9de6-a122ed0421de",
+            device = new
+            {
+                manufacturer = "MPC, PMF & MM Lda",
+                identifiers = new string[] { "ade0b147-b072-4ed0-ad04-8425eade79d8" },
+                model = "Machine Status",
+                name = "Machine Status",
+                sw_version = "1.0.0.0"
+            }
+        };
+
+        return JsonSerializer.Serialize(sensor);
+    }
+
+    private static string GetUptimeSensor()
+    {
+        var sensor = new
+        {
+            name = "Uptime",
+            state_topic = "machinestatus/uptime",
+            icon = "mdi:timer-outline",
+            retain = true,
+            unique_id = "f9e40c5a-41c3-451a-92c7-e8a49aad701e",
+            device = new
+            {
+                manufacturer = "MPC, PMF & MM Lda",
+                identifiers = new string[] { "ade0b147-b072-4ed0-ad04-8425eade79d8" },
+                model = "Machine Status",
+                name = "Machine Status",
+                sw_version = "1.0.0.0"
+            }
+        };
+
+        return JsonSerializer.Serialize(sensor);
     }
 }
